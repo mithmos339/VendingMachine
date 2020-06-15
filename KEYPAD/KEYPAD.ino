1f9ec7325842ee530064b37d727a5e00df05f5a2
monmos339@gmail.com <monmos339@gmail.com>
2020-06-15 09:27:12 +0700

    edit keypad_I2c

--- KEYPAD/KEYPAD.ino
+++ KEYPAD/KEYPAD.ino
@@ -1,4 +1,9 @@
 #include <Keypad.h>
+#include <Keypad_I2C.h>
+#include <Wire.h>
+
+#define I2CADDR 0*20
+
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 char keys[ROWS][COLS] = {
@@ -7,15 +12,20 @@ char keys[ROWS][COLS] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-byte rowPins[ROWS] = {9, 8, 7, 6};
-byte colPins[COLS] = {5, 4, 3, 2};
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+byte rowPins[ROWS] = {0, 1, 2, 3};
+byte colPins[COLS] = {4, 5, 6, 7};
+
+Keypad_I2C kpd( makeKeymap(keys), rowPins, colPins, ROWS, COLS, I2CADDR, PCF8574 );
+
 void setup() {
+  Wire.begin();
+  kpd.begin( makeLeymap(Keys) );
   Serial.begin(9600);
+  Serial.println("Start");
 }
 void loop() {
-  char key = keypad.getKey();
-  if (key != NO_KEY) {
+  char key = kpd.getKey();
+  if (key) {
     Serial.println(key);
   }
 }
